49bda6af5af9b58a84edeb2c1fa98a867d627188
Jamie Sherrah <jamie@ftsy.co>
2019-09-19 15:28:36 +0930

    fixed bug that caused seg fault on macos using sleep; untabified

--- rnnoise.py
+++ rnnoise.py
@@ -1,11 +1,12 @@
 #Author Shoaib Omar
 import ctypes,numpy,os
 from ctypes import util
+import time
 
 lib_path = util.find_library("rnnoise")
 if (not("/" in lib_path)):
-	lib_path = (os.popen('ldconfig -p | grep '+lib_path).read().split('\n')[0].strip().split(" ")[-1] or ("/usr/local/lib/"+lib_path))
-	
+        lib_path = (os.popen('ldconfig -p | grep '+lib_path).read().split('\n')[0].strip().split(" ")[-1] or ("/usr/local/lib/"+lib_path))
+
 lib = ctypes.cdll.LoadLibrary(lib_path)
 lib.rnnoise_process_frame.argtypes = [ctypes.c_void_p,ctypes.POINTER(ctypes.c_float),ctypes.POINTER(ctypes.c_float)]
 lib.rnnoise_process_frame.restype = ctypes.c_float
@@ -13,13 +14,18 @@ lib.rnnoise_create.restype = ctypes.c_void_p
 lib.rnnoise_destroy.argtypes = [ctypes.c_void_p]
 
 class RNNoise(object):
-	def __init__(self):
-		self.obj = lib.rnnoise_create()
-	def process_frame(self,inbuf):
-		outbuf = numpy.ndarray((480,), 'h', inbuf).astype(ctypes.c_float)
-		outbuf_ptr = outbuf.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
-		VodProb =  lib.rnnoise_process_frame(self.obj,outbuf_ptr,outbuf_ptr)
-		return (VodProb,outbuf.astype(ctypes.c_short).tobytes())
+       def __init__(self):
+               # Code seg faults without this.  Seems it needs time for the libraries above to load up or
+               # something!!
+               time.sleep(.01)
+               self.obj = lib.rnnoise_create()
 
-	def destroy(self):
-		lib.rnnoise_destroy(self.obj)
+       def process_frame(self,inbuf):
+               #print('rnnoise: process_frame')
+               outbuf = numpy.ndarray((480,), 'h', inbuf).astype(ctypes.c_float)
+               outbuf_ptr = outbuf.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+               VodProb =  lib.rnnoise_process_frame(self.obj,outbuf_ptr,outbuf_ptr)
+               return (VodProb,outbuf.astype(ctypes.c_short).tobytes())
+
+       def destroy(self):
+               lib.rnnoise_destroy(self.obj)
--- time_test.py
+++ time_test.py
@@ -3,20 +3,24 @@ import time
 import rnnoise
 import numpy as np
 def time_rnnoise(rounds=1000):
-	a = rnnoise.RNNoise()
-	timer = 0.0
-	st = time.time()
-	for i in range(rounds):
-		inp = np.random.bytes(960)
-	timer = (time.time() - st)
-	print(timer)
-	st = time.time()
-	for i in range(rounds):
-		inp = np.random.bytes(960)
-		va,out = a.process_frame(inp)
-	time_taken_per_frame = ((time.time()-st)-timer) /rounds
-	print("time taken for one frame - " + str(time_taken_per_frame ))
-	print("time in a frame - " +str(480.0/48000.0)) 
-	print(str((480.0/48000.0)/time_taken_per_frame )+"X faster than real")
-	a.destroy()
+        a = rnnoise.RNNoise()
+        timer = 0.0
+        st = time.time()
+        for i in range(rounds):
+                inp = np.random.bytes(960)
+        timer = (time.time() - st)
+        print(timer)
+        st = time.time()
+        for i in range(rounds):
+                print('i={}'.format(i))
+                inp = np.random.bytes(960)
+                # what is va?
+                va,out = a.process_frame(inp)
+        time_taken_per_frame = ((time.time()-st)-timer) /rounds
+        print("time taken for one frame - " + str(time_taken_per_frame ))
+        print("time in a frame - " +str(480.0/48000.0))
+        print(str((480.0/48000.0)/time_taken_per_frame )+"X faster than real")
+        a.destroy()
+
+
 time_rnnoise()
